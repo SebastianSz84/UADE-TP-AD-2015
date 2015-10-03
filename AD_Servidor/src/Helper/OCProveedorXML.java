252220bcfab1f14505a2977f0f361106bc3a6a54
Fernandez Dilernia, V. <v.fernandez.dilernia@accenture.com>
2015-10-03 19:41:24 -0300

    nada

--- AD_Servidor/src/Helper/OCProveedorXML.java
+++ AD_Servidor/src/Helper/OCProveedorXML.java
@@ -14,6 +14,7 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import Entities.ItemOCProveedor;
 import Entities.OCProveedor;
 
 public class OCProveedorXML
@@ -24,30 +25,36 @@ public class OCProveedorXML
 		
 		try
 		{
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.newDocument();
-			Element rootElement = doc.createElement("OCProveedor");
-			doc.appendChild(rootElement);
-			Element orden = doc.createElement("orden");
-			rootElement.appendChild(orden);
-			Attr attr = doc.createAttribute("codigo");
-			attr.setValue("Test");
-			orden.setAttributeNode(attr);
-			Element item = doc.createElement("item");
-			Attr attrCant = doc.createAttribute("cantidad");
-			attrCant.setValue("55");
-			item.setAttributeNode(attrCant);
-			Attr attrSKFRod = doc.createAttribute("skf");
-			attrSKFRod.setValue("skf");
-			item.setAttributeNode(attrSKFRod);
-			orden.appendChild(item);
-			
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("C:\\OCProveedor.xml"));
-			transformer.transform(source, result);
+			for (OCProveedor oc : ordenesCompra)
+			{
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.newDocument();
+				Element rootElement = doc.createElement("OCProveedor");
+				doc.appendChild(rootElement);
+				Element orden = doc.createElement("orden");
+				rootElement.appendChild(orden);
+				Attr attr = doc.createAttribute("codigo");
+				attr.setValue(oc.getCodigo());
+				orden.setAttributeNode(attr);
+				for (ItemOCProveedor iocp : oc.getItems())
+				{
+					Element item = doc.createElement("item");
+					Attr attrCant = doc.createAttribute("cantidad");
+					attrCant.setValue(String.valueOf(iocp.getCantidad()));
+					item.setAttributeNode(attrCant);
+					Attr attrSKFRod = doc.createAttribute("skf");
+					attrSKFRod.setValue(iocp.getRodamiento().getCodigoSKF());
+					item.setAttributeNode(attrSKFRod);
+					orden.appendChild(item);
+				}
+				
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(doc);
+				StreamResult result = new StreamResult(new File("C:\\OCProveedor.xml"));
+				transformer.transform(source, result);
+			}
 			
 		}
 		catch (Exception e)
