2df0f3b3094b32e43ef1695d6fc1ec914ea71640
Fernandez Dilernia, V. <v.fernandez.dilernia@accenture.com>
2015-11-21 18:58:10 -0300

    remito/items remito

--- AD_Servidor/src/Entities/ItemRemito.java
+++ AD_Servidor/src/Entities/ItemRemito.java
@@ -1,7 +1,53 @@
 package Entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
 public class ItemRemito
 {
+	@Column
 	private int cantidad;
+	
+	@OneToOne
+	@JoinColumns(@JoinColumn(name = "codigoSKF"))
 	private Rodamiento rodamiento;
+	
+	@ManyToOne
+	@JoinColumn(name = "idRemito")
+	private Remito remito;
+	
+	public int getCantidad()
+	{
+		return cantidad;
+	}
+	
+	public void setCantidad(int cantidad)
+	{
+		this.cantidad = cantidad;
+	}
+	
+	public Rodamiento getRodamiento()
+	{
+		return rodamiento;
+	}
+	
+	public void setRodamiento(Rodamiento rodamiento)
+	{
+		this.rodamiento = rodamiento;
+	}
+	
+	public Remito getRemito()
+	{
+		return remito;
+	}
+	
+	public void setRemito(Remito remito)
+	{
+		this.remito = remito;
+	}
 }
