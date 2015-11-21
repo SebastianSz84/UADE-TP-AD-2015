ce6b8365f77efafce79f4f0a95fbe68b401541c7
Leonardo Esmoris <leonardo.esmoris@axa-assistance.com>
2015-11-21 18:06:57 -0300

    Login y pantalla de mostrar cotizaciones

---
+++ AD_Cliente_Web/src/clientes/servletListarCotizaciones.java
@@ -0,0 +1,33 @@
+package clientes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servletListarCotizaciones extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public servletListarCotizaciones() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ AD_Cliente_Web/src/clientes/servletLogin.java
@@ -0,0 +1,37 @@
+package clientes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servletLogin extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public servletLogin() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String usuario = request.getParameter("nroCliente");
+		
+		// checks if the client exists
+		
+		request.getRequestDispatcher("/Cotizaciones.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
