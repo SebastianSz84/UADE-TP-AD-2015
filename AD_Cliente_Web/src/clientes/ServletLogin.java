8eaaf63a5ba52944d250ff74a93496cf20482c5c
Leonardo Esmoris <leonardo.esmoris@axa-assistance.com>
2015-11-21 19:24:47 -0300

    Fixes

--- AD_Cliente_Web/src/clientes/ServletLogin.java
+++ AD_Cliente_Web/src/clientes/ServletLogin.java
@@ -1,18 +1,13 @@
 package clientes;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 public class ServletLogin extends HttpServlet {
 
@@ -33,6 +28,6 @@ public class ServletLogin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 	}
-
+	
 }
 	
