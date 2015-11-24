04f8d0d078c7cc5eba19d91a61084f9e6da18786
Gustavo Yauny <gustavo.yauny@gmail.com>
2015-11-23 21:37:21 -0300

    web get cotizaciones

--- AD_Cliente_Web/WebContent/js/home/home.controller.js
+++ AD_Cliente_Web/WebContent/js/home/home.controller.js
@@ -2,6 +2,21 @@
  * Created by gusta on 23/11/2015.
  */
 angular.module('distribuidas')
-    .controller('HomeCtrl', function($scope) {
+    .controller('HomeCtrl', function($scope, $http) {
+        $scope.cotizaciones = [];
 
+        $scope.verCotizaciones = function(){
+            $http.post('http://localhost:8080/AD_Cliente_Web/ServletGetCotizacion')
+            .success(function (data) {
+                $scope.cotizaciones = data;
+            }).
+            error(function (data, status) {
+                console.log(data);
+                console.log(status);
+            });
+        };
+
+        $scope.armarCotizacion = function(){
+
+        };
     });
