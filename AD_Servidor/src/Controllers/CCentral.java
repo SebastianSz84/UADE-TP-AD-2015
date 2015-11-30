9139b52d95fece7c552ed3b5e0f17c42c2781205
Fernandez Dilernia, V. <v.fernandez.dilernia@accenture.com>
2015-11-29 21:33:16 -0300

    item proveedor

--- AD_Servidor/src/Controllers/CCentral.java
+++ AD_Servidor/src/Controllers/CCentral.java
@@ -271,7 +271,7 @@ public class CCentral
 			Rodamiento rod = buscarRodamiento(itemProveedorDTO.getSKF());
 			if (rod != null)
 			{
-				ItemProveedor item = proveedor.buscarItem(itemProveedorDTO.getCodRodProv());
+				ItemProveedor item = proveedor.buscarItem(itemProveedorDTO.getCodRodProv(), itemProveedorDTO.getSKF());
 				if (item == null)
 				{
 					proveedor.agregarItem(itemProveedorDTO.getCodRodProv(), itemProveedorDTO.getPrecio(), itemProveedorDTO.getCondiciones(), itemProveedorDTO.getDisponible(), rod);
